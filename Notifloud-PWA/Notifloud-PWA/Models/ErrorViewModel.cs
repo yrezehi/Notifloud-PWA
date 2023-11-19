69560a74cc66043b5bf41f75b8683503997e68d8
unknown <yrezehi@gmail.com>
2023-11-19 06:22:36 -0800

    add: init

---
+++ Notifloud-PWA/Notifloud-PWA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Notifloud_PWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
